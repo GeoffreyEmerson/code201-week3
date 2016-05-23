e6c3e97d14dfb84f6bd3f03168fa7d8b3d426978
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-05-22 20:30:26 -0700

    vote counter works and resized images

--- pic-clicks.js
+++ pic-clicks.js
@@ -5,35 +5,58 @@ function gebi(name) {
 // TODO: Generate this array by reading the img folder
 var pics_array = ['Arya-Stark.jpg', 'Bran-Stark.jpg', 'Cersei-Lannister.jpg', 'Daenerys-Targaryen.jpg', 'Jon-Snow.jpg', 'Sansa-Stark.jpg', 'Tyrion-Lannister.jpg'];
 var current_array = [];
+var votes = {};
 
 // TODO: Pass in pics_array without making it a global
 function swap_pic(event) {
-  var current_pic = gebi(event.target.id).src;
-  current_pic = current_pic.substring(current_pic.lastIndexOf('/') + 1, current_pic.length);
-  console.log('Current pic is: "' + current_pic + '"');
-  var next_pic = current_pic;
-  while (next_pic == current_pic) {
-    next_pic = pics_array[ Math.floor(Math.random() * pics_array.length) ];
+  var position = event.target.id;
+  console.log('Chosen position was: "' + position + '"');
+  var filename = event.target.src;
+  filename = filename.substring(filename.lastIndexOf('/') + 1, filename.length);
+  console.log('Filename chosen was: "' + filename + '"');
+  var image_votes = add_vote(filename);
+  console.log('Total votes for that image is: "' + image_votes + '"');
+
+  if (current_array.length < pics_array.length) {
+    var next_pic = fresh_pic();
+    gebi(event.target.id).src = 'img/' + pics_array[next_pic];
+  } else {
+    console.log('Emptying array and starting over.');
+    populate_images();
   }
-  gebi(event.target.id).src = 'img/' + next_pic;
 }
 
-function populate_images(image_array_arg) {
+function add_vote(key) {
+  if (key in votes) {
+    votes[key] += 1;
+  } else {
+    votes[key] = 1;
+  }
+  return votes[key];
+}
+
+function populate_images() {
+  gebi('page-container').innerHTML = '';
+  current_array = [];
   for (var i = 0; i < 4; i++) {
-    var selection = Math.floor(Math.random() * image_array_arg.length);
-    console.log(current_array.indexOf(selection));
-    while ( current_array.indexOf(selection) != -1) {
-      selection = Math.floor(Math.random() * image_array_arg.length);
-      console.log(current_array.indexOf(selection));
-    }
-    current_array.push(selection); // keep track of what is being shown
+    var selection = fresh_pic();
     var img = document.createElement('img');
     img.setAttribute('class', 'headshot');
     img.setAttribute('id', 'pic' + i);
-    img.setAttribute('src', 'img/' + image_array_arg[selection]);
+    img.setAttribute('src', 'img/' + pics_array[selection]);
+    img.setAttribute('value', selection);
     gebi('page-container').appendChild(img);
     gebi('pic' + i).addEventListener('click', swap_pic);
   }
+}
+
+function fresh_pic() {
+  var selection = Math.floor(Math.random() * pics_array.length);
+  while ( current_array.indexOf(selection) != -1) {
+    selection = Math.floor(Math.random() * pics_array.length);
+  }
+  current_array.push(selection); // keep track of what is being shown
+  return selection;
 }
 
 populate_images(pics_array);
