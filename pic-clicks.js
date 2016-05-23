ea09467c25d9a4255222bab7c09993baeb33c1bd
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-05-22 19:05:55 -0700

    eliminated duplicating from first display

--- pic-clicks.js
+++ pic-clicks.js
@@ -2,11 +2,11 @@ function gebi(name) {
   return document.getElementById(name);
 }
 
+// TODO: Generate this array by reading the img folder
 var pics_array = ['Arya-Stark.jpg', 'Bran-Stark.jpg', 'Cersei-Lannister.jpg', 'Daenerys-Targaryen.jpg', 'Jon-Snow.jpg', 'Sansa-Stark.jpg', 'Tyrion-Lannister.jpg'];
+var current_array = [];
 
-// gebi('pic1').addEventListener('click', swap_pic);
-// gebi('pic2').addEventListener('click', swap_pic);
-
+// TODO: Pass in pics_array without making it a global
 function swap_pic(event) {
   var current_pic = gebi(event.target.id).src;
   current_pic = current_pic.substring(current_pic.lastIndexOf('/') + 1, current_pic.length);
@@ -18,20 +18,22 @@ function swap_pic(event) {
   gebi(event.target.id).src = 'img/' + next_pic;
 }
 
-function populate_images() {
-  var display_array = [];
+function populate_images(image_array_arg) {
   for (var i = 0; i < 4; i++) {
-    var selection = Math.floor(Math.random() * pics_array.length);
-    while ( display_array.indexOf(selection) != -1) {
-      selection = Math.floor(Math.random() * pics_array.length);
+    var selection = Math.floor(Math.random() * image_array_arg.length);
+    console.log(current_array.indexOf(selection));
+    while ( current_array.indexOf(selection) != -1) {
+      selection = Math.floor(Math.random() * image_array_arg.length);
+      console.log(current_array.indexOf(selection));
     }
+    current_array.push(selection); // keep track of what is being shown
     var img = document.createElement('img');
     img.setAttribute('class', 'headshot');
     img.setAttribute('id', 'pic' + i);
-    img.setAttribute('src', 'img/' + pics_array[selection]);
+    img.setAttribute('src', 'img/' + image_array_arg[selection]);
     gebi('page-container').appendChild(img);
     gebi('pic' + i).addEventListener('click', swap_pic);
   }
 }
 
-populate_images();
+populate_images(pics_array);
