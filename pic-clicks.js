09da91777672d7b070774084cd8472f5a0449a3e
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-05-23 14:18:27 -0700

    converted to objects and removed wasted random() calls

--- pic-clicks.js
+++ pic-clicks.js
@@ -1,63 +1,51 @@
+// array of Item objects
+var choices = [];
+
 // track what has been shown this round
-var current_array = [];
+// this array will start out full and choices get removed as they are used
+var available_choices = [];
 
-// track votes over the whole session
-var votes = {};
-
-// TODO: Generate this array by reading the img folder
+// list of given pics in the img directory
 var pics_array = ['Arya-Stark.jpg', 'Bran-Stark.jpg', 'Cersei-Lannister.jpg', 'Daenerys-Targaryen.jpg', 'Jon-Snow.jpg', 'Sansa-Stark.jpg', 'Tyrion-Lannister.jpg'];
 
 //start the game here
+load_objects(pics_array);
 populate_images(pics_array);
 
-// When a click is detected, log the vote and swap in a new pic
-// TODO: Pass in pics_array without making it a global
-function swap_pic(event) {
-  var filename = event.target.src;
-  filename = filename.substring(filename.lastIndexOf('/') + 1, filename.length);
-  var image_votes = add_vote(filename);
+function Item(src) {
+  this.src = 'img/' + src;
+  this.name = src.slice(0,src.lastIndexOf('.')).replace('-',' ');
+  this.clicks = 0;
 
-  if (current_array.length < pics_array.length) {
-    // Show a new pic
-    var next_pic = fresh_pic();
-    gebi(event.target.id).src = 'img/' + pics_array[next_pic];
-    var vote_count;
-    if ( !(vote_count = votes[pics_array[next_pic]]) ) {
-      vote_count = 0;
-    }
-    event.target.nextSibling.textContent = pics_array[selection] + ': ' + vote_count + ' clicks';
-  } else {
-    // Empty array and start over
-    populate_images();
-  }
+  this.addClick = function() {
+    this.clicks++;
+  };
 }
 
-function add_vote(key) {
-  if (key in votes) {
-    votes[key] += 1;
-  } else {
-    votes[key] = 1;
+function load_objects(name_array) {
+  for (var i = 0; i < name_array.length; i++) {
+    var item = new Item(name_array[i]);
+    choices.push(item);
   }
-  return votes[key];
 }
 
 function populate_images() {
   gebi('page-container').innerHTML = '';
-  current_array = [];
+  // when the game gets reset, all choice objects are available to be picked.
+  for (var i = 0; i < choices.length; i++) {
+    available_choices.push(i);
+  }
+  // now make random selections for the initial 4 images
   for (var i = 0; i < 4; i++) {
     var selection = fresh_pic();
     var img = document.createElement('img');
     img.setAttribute('class', 'headshot');
     img.setAttribute('id', 'pic' + i);
-    img.setAttribute('src', 'img/' + pics_array[selection]);
-    img.setAttribute('value', selection);
+    img.setAttribute('src', choices[selection].src);
+    img.setAttribute('choice', selection);
     var caption = document.createElement('figcaption');
     caption.setAttribute('id', 'vote' + i);
-    var vote_count;
-    if ( !(vote_count = votes[pics_array[selection]]) ) {
-      vote_count = 0;
-    }
-    var text = document.createTextNode(pics_array[selection] + ': ' + vote_count + ' clicks');
+    var text = document.createTextNode(choices[selection].name + ': ' + choices[selection].clicks + ' clicks');
     caption.appendChild(text);
     var pic_div = document.createElement('div');
     pic_div.appendChild(img);
@@ -67,12 +55,25 @@ function populate_images() {
   }
 }
 
+// When a click is detected, log the vote and swap in a new pic
+function swap_pic(event) {
+  choices[event.target.attributes[3].value].addClick();
+  if (available_choices.length > 0) {
+    var next_pic = fresh_pic();
+    var pic_element = gebi(event.target.id);
+    pic_element.src = choices[next_pic].src;
+    pic_element.setAttribute('choice', next_pic);
+    event.target.nextSibling.textContent = choices[next_pic].name + ': ' + choices[next_pic].clicks + ' clicks';
+  } else {
+    populate_images();
+  }
+}
+
 function fresh_pic() {
-  do {
-    selection = Math.floor(Math.random() * pics_array.length);
-  } while (current_array.indexOf(selection) != -1);
-  current_array.push(selection); // keep track of what is being shown
-  return selection;
+  var choice_index = Math.floor(Math.random() * available_choices.length);
+  var choice = available_choices[choice_index];
+  available_choices.splice(choice_index,1); // eliminate the choice used
+  return choice;
 }
 
 // utility functions
