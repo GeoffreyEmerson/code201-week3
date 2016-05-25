f273d414940dd813efee52e1301a3375f86b025a
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-05-25 15:25:17 -0700

    successfully storing and recovering image objects and total clicks

--- pic-clicks.js
+++ pic-clicks.js
@@ -13,7 +13,7 @@ var available_choices = [];
 
 // track how many times the user has made a choices
 var total_clicks = 0;
-var bonus_round = false;
+var bonus_round;
 
 // list of given pics in the img directory
 var pics_array = ['Arya-Stark.jpg',
@@ -31,47 +31,54 @@ var pics_array = ['Arya-Stark.jpg',
                   'Tommen-Baratheon.jpg',
                   'Tyrion-Lannister.jpg'];
 
-//start the game here
+//start the app here
 load_objects(pics_array);
 preload_images(choices);
-render_image_containers();
-render_buttons();
-populate_images(pics_array);
+render_image_containers(); // the container div is in the html and is never removed, only cleared on restart
 
-function Item(src, clicks) {
+load_state();
+
+// these should probably go into load_state
+render_buttons();
+
+function Item(src, clicks, times_shown) {
   this.src = src;
-  this.clicks = 0;
-  this.times_shown = 0;
+  this.clicks = clicks || 0;
+  console.log(this.clicks);
+  this.times_shown = times_shown || 0;
+  console.log(this.times_shown);
 
   this.name = function() {
     return src.slice(0,src.lastIndexOf('.')).replace(/-/g,' ');
   };
 
+  this.save_state = function() {
+    localStorage[this.src] = JSON.stringify(this);
+  };
+
   this.add_click = function() {
     this.clicks++;
+    this.save_state();
   };
 
   this.increment_times_shown = function() {
     this.times_shown++;
+    this.save_state();
   };
+
+  this.save_state();
 }
 
 function load_objects(name_array) {
-  var stored_object_array;
-  if (stored_object_array = localStorage.image_object_array) {
-    stored_object_array = JSON.parse(stored_object_array);
-  } else {
-    for (var i = 0; i < name_array.length; i++) {
+  for (var i = 0; i < name_array.length; i++) {
+    var temp_object_raw = localStorage[name_array[i]];
+    if (temp_object_raw) {
+      var temp_object = JSON.parse(temp_object_raw);
+      var item = new Item(temp_object.src,temp_object.clicks,temp_object.times_shown);
+    } else {
       var item = new Item(name_array[i]);
-      choices.push(item);
     }
-  }
-}
-
-function preload_images(obj_array) {
-  for (var i = 0; i < obj_array.length; i++) {
-    var temp_image = new Image();
-    temp_image.src = 'img/' + obj_array[i].src;
+    choices.push(item);
   }
 }
 
@@ -139,21 +146,68 @@ function render_buttons() {
   document.body.appendChild(button_div);
 }
 
-function populate_images() {
-  // now make random selections for the initial 4 images
+// When the app first loads, check for saved state variables
+function load_state() {
+  var last_shown = localStorage.last_shown;
+  if (last_shown) {
+    last_shown = JSON.parse(last_shown);
+    populate_images(last_shown);
+  } else {
+    populate_images();
+  }
+  var stored_clicks = localStorage.total_clicks;
+  if (stored_clicks && stored_clicks != 'NaN') {
+    total_clicks = stored_clicks;
+  }
+
+  var stored_round = localStorage.bonus_round;
+  if (stored_round) {
+    bonus_round = stored_round;
+  } else {
+    bonus_round = false;
+  }
+
+  var stored_display_state = localStorage.display_state;
+  if (stored_display_state) {
+    display_state = stored_display_state;
+  } else {
+    display_state = 0;
+  }
+  display_app();
+}
+
+function display_app(state) {
+  // State 0 is just the Heading, Instructions, and three images
+
+  // State 1 adds the display of the two choice buttons
+
+  // State 2
+
+}
+
+// display 3 fresh images
+function populate_images(saved) {
+  var showing = [];
   for (var i = 0; i < NUM_PICS_DISPLAYED; i++) {
-    var selection = fresh_pic();
+    if (saved) {
+      selection = saved[i];
+    } else {
+      var selection = fresh_pic();
+    }
     var img = gebi('pic' + i);
     img.setAttribute('src', 'img/' + choices[selection].src);
     img.setAttribute('choice', selection);
     var caption = gebi('vote' + i);
     caption.textContent = choices[selection].name();
+
+    showing[i] = selection;
   }
 }
 
 // When a click is detected, log the vote and swap in a new pic
 function swap_pic(event) {
   total_clicks++;
+  save_total_clicks();
   choices[event.target.attributes[3].value].add_click();
   check_finished();
 }
@@ -256,9 +310,14 @@ function restart() {
   render_image_containers();
   render_buttons();
   total_clicks = 0;
+  save_total_clicks();
   bonus_round = false;
-  populate_images(pics_array);
+  populate_images();
   smooth_scroll_to(document.body);
+}
+
+function save_total_clicks() {
+  localStorage.total_clicks = total_clicks;
 }
 
 function fresh_pic() {
@@ -273,6 +332,13 @@ function fresh_pic() {
   available_choices.splice(choice_index,1); // eliminate the choice used
   choices[choice].increment_times_shown();
   return choice;
+}
+
+function preload_images(obj_array) {
+  for (var i = 0; i < obj_array.length; i++) {
+    var temp_image = new Image();
+    temp_image.src = 'img/' + obj_array[i].src;
+  }
 }
 
 // utility functions
